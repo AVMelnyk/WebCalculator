894ec3ced6742e21f95260babd1fa255a3b45752
AVMelnyk <melnyk.andreew@gmail.com>
2017-05-06 00:23:14 +0300

    new commit

--- src/main/java/com/avmelnyk/Page.java
+++ src/main/java/com/avmelnyk/Page.java
@@ -42,6 +42,90 @@ public class Page extends WebPage{
                     message = message.concat("3");
             }
         });
+        add(new Link("link4") {
+            @Override
+            public void onClick() {
+                if (message.equals("0")){
+                    message = "4";
+                }
+                else
+                    message = message.concat("4");
+            }
+        });
+        add(new Link("link5") {
+            @Override
+            public void onClick() {
+                if (message.equals("0")){
+                    message = "5";
+                }
+                else
+                    message = message.concat("5");
+            }
+        });
+        add(new Link("link6") {
+            @Override
+            public void onClick() {
+                if (message.equals("0")){
+                    message = "6";
+                }
+                else
+                    message = message.concat("7");
+            }
+        });
+        add(new Link("link7") {
+            @Override
+            public void onClick() {
+                if (message.equals("0")){
+                    message = "7";
+                }
+                else
+                    message = message.concat("7");
+            }
+        });
+        add(new Link("link8") {
+            @Override
+            public void onClick() {
+                if (message.equals("0")){
+                    message = "8";
+                }
+                else
+                    message = message.concat("8");
+            }
+        });
+        add(new Link("link9") {
+            @Override
+            public void onClick() {
+                if (message.equals("0")){
+                    message = "9";
+                }
+                else
+                    message = message.concat("9");
+            }
+        });
+        add(new Link("divide") {
+            @Override
+            public void onClick() {
+            	message = message.concat(" / ");
+            }
+        });
+        add(new Link("multiply") {
+            @Override
+            public void onClick() {
+            	message = message.concat(" x ");
+            }
+        });
+        add(new Link("minus") {
+            @Override
+            public void onClick() {
+            	message = message.concat(" - ");
+            }
+        });
+        add(new Link("plus") {
+            @Override
+            public void onClick() {
+            	message = message.concat(" + ");
+            }
+        });
         add(new Label("label", new PropertyModel(this, "message")));
     }
 }
