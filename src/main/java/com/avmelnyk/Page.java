e39620d62751b6c3b213d7f43cf37bca93c98d02
AVMelnyk <melnyk.andreew@gmail.com>
2017-05-06 22:48:09 +0300

    replacOperator feature have been added

--- src/main/java/com/avmelnyk/Page.java
+++ src/main/java/com/avmelnyk/Page.java
@@ -8,159 +8,187 @@ import org.apache.wicket.markup.html.link.ExternalLink;
 import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 
-
 public class Page extends WebPage{
-    String message = "0";
+    String output = "0";
     public Page() {
         add(new Link("link") {
             @Override
             public void onClick() {
-                if (message.equals("0")){
-                    message = "1";
+                if (output.equals("0")){
+                    output = "1";
                 }
                else
-                   message = message.concat("1");
+                   output = output.concat("1");
             }
         });
         
         add(new Link("link2") {
             @Override
             public void onClick() {
-                if (message.equals("0")){
-                    message = "2";
+                if (output.equals("0")){
+                    output = "2";
                 }
                 else
-                    message = message.concat("2");
+                    output = output.concat("2");
             }
         });
         add(new Link("link3") {
             @Override
             public void onClick() {
-                if (message.equals("0")){
-                    message = "3";
+                if (output.equals("0")){
+                    output = "3";
                 }
                 else
-                    message = message.concat("3");
+                    output = output.concat("3");
             }
         });
         add(new Link("link4") {
             @Override
             public void onClick() {
-                if (message.equals("0")){
-                    message = "4";
+                if (output.equals("0")){
+                    output = "4";
                 }
                 else
-                    message = message.concat("4");
+                    output = output.concat("4");
             }
         });
         add(new Link("link5") {
             @Override
             public void onClick() {
-                if (message.equals("0")){
-                    message = "5";
+                if (output.equals("0")){
+                    output = "5";
                 }
                 else
-                    message = message.concat("5");
+                    output = output.concat("5");
             }
         });
         add(new Link("link6") {
             @Override
             public void onClick() {
-                if (message.equals("0")){
-                    message = "6";
+                if (output.equals("0")){
+                    output = "6";
                 }
                 else
-                    message = message.concat("6");
+                    output = output.concat("6");
             }
         });
         add(new Link("link7") {
             @Override
             public void onClick() {
-                if (message.equals("0")){
-                    message = "7";
+                if (output.equals("0")){
+                    output = "7";
                 }
                 else
-                    message = message.concat("7");
+                    output = output.concat("7");
             }
         });
         add(new Link("link8") {
             @Override
             public void onClick() {
-                if (message.equals("0")){
-                    message = "8";
+                if (output.equals("0")){
+                    output = "8";
                 }
                 else
-                    message = message.concat("8");
+                    output = output.concat("8");
             }
         });
         add(new Link("link9") {
             @Override
             public void onClick() {
-                if (message.equals("0")){
-                    message = "9";
+                if (output.equals("0")){
+                    output = "9";
                 }
                 else
-                    message = message.concat("9");
+                    output = output.concat("9");
             }
         });
         add(new Link("null") {
             @Override
             public void onClick() {
-                
+            	output= output.concat("0");
             }
         });
         add(new Link("cancel") {
             @Override
             public void onClick() {
-            	if (message.length()>1) {
-            		 message = message.substring(0, message.length()-1);
+            	if (output.length()>1) {
+            		 output = output.substring(0, output.length()-1);
             	}
             	else{
-            		message = "0";
+            		output = "0";
             	}
             }
         });
         
         add(new Link("divide") {
+        	
             @Override
             public void onClick() {
-            	message = message.concat("\n / ");
-            }
-        });
+            	String divide = " / ";
+	        	output = replaceOperator(output, divide);
+        }});
         add(new Link("multiply") {
             @Override
             public void onClick() {
-            	message = message.concat("\n x ");
+            	String multiply = " x ";
+	        	output = replaceOperator(output, multiply);
             }
         });
         add(new Link("minus") {
             @Override
             public void onClick() {
-            	message = message.concat("\n - ");
+            	String minus = " - ";
+            	output = replaceOperator(output, minus);
             }
         });
         add(new Link("dot") {
             @Override
             public void onClick() {
-            	message = message.concat(".");
+            	output = output.concat(".");
             }
         });
         add(new Link("equally") {
             @Override
             public void onClick() {
-            	message = message.concat("=");
+            	output = output.concat(" = ");
             }
         });
         add(new Link("plus") {
             @Override
             public void onClick() {
-            	message = message.concat("\n + ");
+            	String plus = " + ";
+            	output = replaceOperator(output, plus);
             }
         });
-        add(new MultiLineLabel("label", new PropertyModel(this, "message")));
+        add(new MultiLineLabel("label", new PropertyModel(this, "output")));
+    }
+    
+    public static String replaceOperator(String output, String operator){
+    	String lastElem = output.substring(output.length() -1, output.length());
+    	if(isNumeric(lastElem)){
+    		output = output.concat(operator);
+    	}
+    	else{
+    		String  newoutput = output.substring(0, output.length()-3);
+    		 output = newoutput.concat(operator);
+    	}
+    	return output;
+    	
+    }
+    
+    public static boolean isNumeric(String str)  
+    {  
+      try  
+      {  
+        double d = Double.parseDouble(str);  
+      }  
+      catch(NumberFormatException nfe)  
+      {  
+        return false;  
+      }  
+      return true;  
     }
 }
